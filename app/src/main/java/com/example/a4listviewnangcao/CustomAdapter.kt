6cdad18eefdfc9174a875dfbd0eba8b2fdf935c7
Phan Mỹ Linh <havana03022003@gmail.com>
2023-03-03 22:45:52 +0700

    update

--- app/src/main/java/com/example/a4listviewnangcao/CustomAdapter.kt
+++ app/src/main/java/com/example/a4listviewnangcao/CustomAdapter.kt
@@ -1,54 +1,43 @@
 package com.example.a4listviewnangcao
 
+import android.app.Activity
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
 import java.text.FieldPosition
 
-class CustomAdapter constructor(var context :Context, var mangmonan : ArrayList<MonAn>) : BaseAdapter() {
-    class ViewHolder (row:View){
-        var textviewTenmonan : TextView
-        var imgviewMonan : ImageView
+class CustomAdapter constructor(var context :Activity, var mang : ArrayList<MonAn>) : ArrayAdapter<MonAn>(context, R.layout.dong_monan) {
+//    class ViewHolder (row:View){
+//        var textviewTenmonan : TextView
+//        var imgviewMonan : ImageView
+//
+//        init{
+//            textviewTenmonan = row.findViewById(R.id.tviewMonan)
+//            imgviewMonan = row.findViewById(R.id.imgviewHinhmonan)
+//        }
+//    }
 
-        init{
-            textviewTenmonan = row.findViewById(R.id.tviewMonan)
-            imgviewMonan = row.findViewById(R.id.imgviewHinhmonan)
-        }
-    }
 
     override fun getCount(): Int {
-        return mangmonan.size
-        //ve so dong bang so gia tri tra ve
+        return mang.size
+        //vẽ lên view hết tất cả các dòng của list
     }
 
-    override fun getItem(position: Int): Any {
-        return mangmonan.get(position)
-        //chi ra cac thuoc tinh trong doi tuong
-    }
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var context1 = context.layoutInflater
+        //layoutInflater là 1 component giúp chuyển layout xml thành view trong android
+        var rowView = context1.inflate(R.layout.dong_monan, parent, false)
 
-    override fun getItemId(p0: Int): Long {
-        return p0.toLong()
-    }
+        var img = rowView.findViewById<ImageView>(R.id.img)
+        var name = rowView.findViewById<TextView>(R.id.name)
 
-    override fun getView(position: Int, convertView: View?, p2: ViewGroup?): View {
-        var view: View?
-        var viewHolder : ViewHolder
-        if(convertView == null){
-            var layinflater :LayoutInflater = LayoutInflater.from(context)
-            view = layinflater.inflate(R.layout.dong_monan, null)
-            viewHolder = ViewHolder(view)
-            view.tag = viewHolder
-        }else{
-            view = convertView
-            viewHolder = convertView.tag as ViewHolder
-        }
-        var monan :MonAn = getItem(position) as MonAn
-        viewHolder.textviewTenmonan.text = monan.ten
-        viewHolder.imgviewMonan.setImageResource(monan.hinhanh)
-        return view as View
+        name.text = mang.get(position).ten
+        img.setImageResource(mang[position].hinhanh)
+        return rowView
     }
 }
--- app/src/main/java/com/example/a4listviewnangcao/MainActivity.kt
+++ app/src/main/java/com/example/a4listviewnangcao/MainActivity.kt
@@ -2,12 +2,18 @@ package com.example.a4listviewnangcao
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ListView
+import android.widget.AdapterView
+import android.widget.Toast
+
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var customAdapter: CustomAdapter
+    //lateinit var: biến khởi tạo muộn: không cần gán giá trị
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //khai báo danh sách item
         var arrayMonan : ArrayList<MonAn> = ArrayList()
         arrayMonan.add(MonAn("I am strong",R.drawable.h7))
         arrayMonan.add(MonAn("I believe in myself",R.drawable.h6))
@@ -17,7 +23,16 @@ class MainActivity : AppCompatActivity() {
         arrayMonan.add(MonAn("I am smart",R.drawable.h1))
         arrayMonan.add(MonAn("I love myself",R.drawable.h2))
 
-        var listview  = findViewById<ListView>(R.id.listView)
-        listview.adapter = CustomAdapter(this@MainActivity, arrayMonan)
+        customAdapter = CustomAdapter(this, arrayMonan)
+        listView.adapter = customAdapter
+
+        listView.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, i, l ->
+            //adapterView:AdapterView nơi nhấp chuột xảy ra
+            //view:View chế độ xem trong chế độ xem bộ điều hợp đã được nhấp(là chế độ xem do bộ điều hợp cung cấp)
+            //i:Int vị trí của khung nhìn trong bộ điều hợp
+            //l:long id hàng của mục mà bạn đã nhấp
+            Toast.makeText(this,customAdapter.mang[i].ten,Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
